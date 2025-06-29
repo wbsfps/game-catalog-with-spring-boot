f958a44d0ea667cb277406c6eabd20b95029337c
William Santana <williamandrade1058@gmail.com>
2025-06-28 22:52:45 -0300

    feat: implemenet exceptions

---
+++ src/main/java/br/com/wbs/global/exceptions/handler/CustomEntityResponseHandler.java
@@ -0,0 +1,34 @@
+package br.com.wbs.global.exceptions.handler;
+
+import br.com.wbs.global.exceptions.ExceptionResponse;
+import br.com.wbs.global.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice
+public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception exception, WebRequest request) {
+        ExceptionResponse response =
+                new ExceptionResponse(new Date(), exception.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleGameNotFoundExceptions(Exception exception, WebRequest request) {
+        ExceptionResponse response =
+                new ExceptionResponse(new Date(), exception.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
