554892c1fc483dffcf97e8870267e6fc96dbe236
William Santana <williamandrade1058@gmail.com>
2025-07-22 08:50:59 -0300

    up project

--- src/main/java/br/com/wbs/domain/game/GameRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.wbs.domain.game;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface GameRepository extends JpaRepository<Game, UUID> {
-}
