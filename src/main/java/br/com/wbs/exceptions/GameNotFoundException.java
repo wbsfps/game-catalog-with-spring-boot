dd3c258c0f3077861e4025f5e5b8eb49ea587eb0
William Santana <williamandrade1058@gmail.com>
2025-07-22 14:06:15 -0300

    feat: create custom exception game not found

---
+++ src/main/java/br/com/wbs/exceptions/GameNotFoundException.java
@@ -0,0 +1,11 @@
+package br.com.wbs.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class GameNotFoundException extends RuntimeException {
+    public GameNotFoundException() {
+        super("Game not found.");
+    }
+}
