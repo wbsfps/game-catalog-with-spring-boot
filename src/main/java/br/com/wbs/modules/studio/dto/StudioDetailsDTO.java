fdcfe3a70869b542202b539aa8e21bc0cbecc9a0
William Santana <williamandrade1058@gmail.com>
2025-07-23 13:15:31 -0300

    feat: include set of games in response

--- src/main/java/br/com/wbs/modules/studio/dto/StudioDetailsDTO.java
+++ src/main/java/br/com/wbs/modules/studio/dto/StudioDetailsDTO.java
@@ -1,12 +1,14 @@
 package br.com.wbs.modules.studio.dto;
 
+import br.com.wbs.modules.game.entity.GameEntity;
 import br.com.wbs.modules.studio.entity.StudioEntity;
 
+import java.util.Set;
 import java.util.UUID;
 
-public record StudioDetailsDTO(UUID id, String name, Integer quantityGames) {
+public record StudioDetailsDTO(UUID id, String name, Integer quantityGames, Set<GameEntity> games) {
 
     public StudioDetailsDTO(StudioEntity entity) {
-        this(entity.getId(), entity.getName(), entity.getQuantityGames());
+        this(entity.getId(), entity.getName(), entity.getQuantityGames(), entity.getGames());
     }
 }
